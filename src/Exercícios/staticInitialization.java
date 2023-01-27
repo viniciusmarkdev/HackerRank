9b6085ae9836a17c1c2db03593b8b8679119aec2
Marcos Vinicius <contato.marcoscandido@gmail.com>
2023-01-27 18:29:01 -0300

    atualizacao

--- src/Exercícios/staticInitialization.java
+++ src/Exercícios/staticInitialization.java
@@ -3,48 +3,63 @@ package Exercícios;
 import java.util.Scanner;
 
 public class staticInitialization {
-	
-	public static void main(String[] args) {
-		
-		Estatica teste = new Estatica();
-	
+	/*
+	 * Váriaveis estáticas que são inicializadas antes 
+	 * de tudo.
+	 * */
+	static boolean flag = true;
+	static int B, H;
+
+	/*Bloco estático inicializado 
+	 * 
+	 * Os Blocos estáticos servem para que possamos inicializar 
+	 * varíaveis já com algum tratamento de erros ou looping.
+	 * 
+	 * Assim como fazemos com construtores com sua Lógica interna.
+	 * 
+	 * */
+	static {
+	    
+	    Scanner scan = new Scanner(System.in);
+	    B = scan.nextInt();
+	    scan.nextLine();
+	    H = scan.nextInt();
+	    scan.close();
+	    
+	    
+	    if(B>0 && H>0){
+	        flag = true;
+	    }
+	    /*
+	     * Se isso B for < 0 e H < 0 não iremos executar o código 
+	     * do método main e imprimiremos a exceção. Pois o código 
+	     * do main só roda caso a flag seja verdadeira.
+	     * 
+	     * 
+	     * */
+	    else if(B<=0 || H <=0){
+	        flag = false;
+	        System.out.println("java.lang.Exception: Breadth and height must be positive");
+	    }
 	}
 
-}
+	public static void main(String[] args){
+	        if(flag){
+	            int area=B*H;
+	            System.out.print(area);
+	        }
+	        
+	    }//end of main
 
- class Estatica{
-	 
-	 static {
-		 
-		  Scanner entrada1 = new Scanner(System.in);
-		  int H = entrada1.nextInt();
-		  
-		  
-		  Scanner entrada2 = new Scanner(System.in);
-		  int B = entrada2.nextInt();
-		  
-		 
-			  
-			  if(B<=0 || H<=0 ) {
-				  
-				  System.out.println("java.lang.Exception: Breadth and height must be positive");
-				  
-			  }
-			  else {
-				  
-				 
-				int area = H*B;
-				System.out.println(area);
-			  }
-			  
-				  
-			  
-		  }
-		  
-		  
-		  
-	 }
+	}//end of class
+
+
+
  
+		  
+		  
+		  
+	 
  
  
 
