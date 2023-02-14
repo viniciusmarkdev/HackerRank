b2065dced20dfa3e6ed6ef00acebca970be19c63
Marcos Vinicius <contato.marcoscandido@gmail.com>
2023-02-14 19:18:44 -0300

    Split tokens

---
+++ src/Exercícios/StringTokensSplit.java
@@ -0,0 +1,45 @@
+package Exercícios;
+
+import java.util.Scanner;
+
+public class StringTokensSplit {
+	
+	 public static void main(String[] args) {
+	       
+		    Scanner scan = new Scanner(System.in);
+	        String s = scan.nextLine();
+	      
+	        scan.close();
+	        
+	        String [] letras  = s.split("[!,?._'@ ]+");
+	        
+	       int k =0 ;
+           for(String letra:letras) {
+	        	
+	        	
+	        	
+	            
+	        	k++;
+	        	
+	        	
+	        }
+	        
+	        System.out.println(k);
+	        
+	        
+	       
+	        for(String letra:letras) {
+	        	
+	        	
+	        	
+	            
+	        	System.out.println(letra);
+	        	
+	        	
+	        }
+	       
+	 }
+	   
+
+
+}
