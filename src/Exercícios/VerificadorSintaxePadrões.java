b077503e7fea5e33e088c31b18ba52f8988f32af
Marcos Vinicius <contato.marcoscandido@gmail.com>
2023-02-16 09:20:23 -0300

    comentarios

--- src/Exercícios/VerificadorSintaxePadrões.java
+++ src/Exercícios/VerificadorSintaxePadrões.java
@@ -12,13 +12,20 @@ public class VerificadorSintaxePadrões {
 	public static void main(String[] args){
 		Scanner in = new Scanner(System.in);
 		int testCases = Integer.parseInt(in.nextLine());
+		
 		while(testCases>0){
-            try{
+           //Execute o bloco
+			try{
                 String pattern = in.nextLine();
                 Pattern.compile(pattern);
+             
                 System.out.println("Valid");
+              //Caso seja lançada uma execeção despare uma mensagem na tela
+                
             }catch(PatternSyntaxException e){
-                System.out.println("Invalid");
+             
+            	System.out.println("Invalid");
+            	//Se tudo der cero decremente  a váriavel testCases
             }finally{
                 testCases--;
             }           
